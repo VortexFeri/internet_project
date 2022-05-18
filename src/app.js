dce3fa72699e3c90a881b5ca910730f738ed5f4b
Ferencz Carnu <fericarnu@gmail.com>
2022-05-19 00:24:11 +0300

    added About Me section

--- src/app.js
+++ src/app.js
@@ -4,13 +4,19 @@ const header = document.querySelector('header');
 const nav = document.querySelector('nav');
 const root = document.querySelector(':root');
 const arrow = document.querySelector('#scroll-link img');
+const githubIcon = document.querySelector("img[alt='Github']");
+const codepenIcon = document.querySelector("img[alt='Codepen']");
 
 themeSwitch.addEventListener('click', () => {
     themeSwitch.classList.toggle('dark');
     header.style.setProperty('background-image', 'url("/assets/dark2.jpg")');
     root.style.setProperty('--background', '#333');
     root.style.setProperty('--text', '#ccc');
-    arrow.setAttribute('src', '/assets/arrow-light.png')
+    root.style.setProperty('--bg', '#333340');
+    arrow.setAttribute('src', '/assets/arrow-light.png');
+    githubIcon.setAttribute('src', '/assets/github-white.png')
+    codepenIcon.setAttribute('src', '/assets/codepen-white.png')
+
 })
 
 colorSwitch.addEventListener('click', () => {
@@ -19,11 +25,6 @@ colorSwitch.addEventListener('click', () => {
 })
 
 const navLinks = document.querySelectorAll('.nav-links a');
-
-function wrap(el, wrapper) {
-    el.parentNode.insertBefore(wrapper, el);
-    wrapper.appendChild(el);
-}
 
 navLinks.forEach(link => {
     var oldText = link.innerHTML;

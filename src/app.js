2949bc3352ba70394c7b816a49570cbbe5315e49
Ferencz Carnu <fericarnu@gmail.com>
2022-05-20 00:31:16 +0300

    Made the dark mode switcher actually work and broke down the CSS in smaller files

--- src/app.js
+++ src/app.js
@@ -1,29 +1,40 @@
 const themeSwitch = document.getElementById('theme-switch');
 const colorSwitch = document.getElementById('color-switch');
 const header = document.querySelector('header');
-const nav = document.querySelector('nav');
 const root = document.querySelector(':root');
 const arrow = document.querySelector('#scroll-link img');
-const githubIcon = document.querySelector("img[alt='Github']");
-const codepenIcon = document.querySelector("img[alt='Codepen']");
-const linkedinIcon = document.querySelector("img[alt='Linkedin']");
+const icons = document.querySelectorAll('.social img');
 
 themeSwitch.addEventListener('click', () => {
     themeSwitch.classList.toggle('dark');
-    header.style.setProperty('background-image', 'url("/assets/dark2.jpg")');
-    root.style.setProperty('--background', '#333');
-    root.style.setProperty('--text', '#ccc');
-    root.style.setProperty('--bg', '#333340');
-    arrow.setAttribute('src', '/assets/arrow-light.png');
-    githubIcon.setAttribute('src', '/assets/github-white.png')
-    codepenIcon.setAttribute('src', '/assets/codepen-white.png')
-    linkedinIcon.setAttribute('src', '/assets/linkedin-white.png')
+    root.classList.toggle('dark');
+        
+    if (root.classList.contains('dark')) {
+        arrow.setAttribute('src', '/assets/arrow-light.png');
+        header.style.setProperty('background-image', 'url("/assets/dark2.jpg")');
+        icons.forEach(icon => {
+            let aux = icon.getAttribute('src');
+            aux = aux.substring(0, aux.length - 4) + "-white" + aux.substring(aux.length - 4, aux.length);
+            icon.setAttribute('src', aux);
+        })
+    }
+    else {
+        arrow.setAttribute('src', '/assets/arrow-dark.png');
+        header.style.setProperty('background-image', 'url("/assets/pexels-henry-&-co-1939485.jpg")');
+        icons.forEach(icon => {
+            let aux = icon.getAttribute('src');
+            aux = aux.substring(0, aux.length - 10) + "" + aux.substring(aux.length - 4, aux.length);
+            icon.setAttribute('src', aux);
+        })
+    }
 
 })
 
 colorSwitch.addEventListener('click', () => {
-    root.style.setProperty('--accent', '#00b33c');
-    root.style.setProperty('--accent-rgb', '0, 179, 60');
+    if (root.getAttribute('data-color-theme') == 'magenta')
+        root.setAttribute('data-color-theme', 'green');
+    else
+        root.setAttribute('data-color-theme', 'magenta')
 })
 
 const animLinks = document.querySelectorAll('.animated-link');

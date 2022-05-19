8973769edcedb12edf9b1fc996d6333ee2079ee4
Ferencz Carnu <fericarnu@gmail.com>
2022-05-19 14:14:50 +0300

    Added footer

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const root = document.querySelector(':root');
 const arrow = document.querySelector('#scroll-link img');
 const githubIcon = document.querySelector("img[alt='Github']");
 const codepenIcon = document.querySelector("img[alt='Codepen']");
+const linkedinIcon = document.querySelector("img[alt='Linkedin']");
 
 themeSwitch.addEventListener('click', () => {
     themeSwitch.classList.toggle('dark');
@@ -16,6 +17,7 @@ themeSwitch.addEventListener('click', () => {
     arrow.setAttribute('src', '/assets/arrow-light.png');
     githubIcon.setAttribute('src', '/assets/github-white.png')
     codepenIcon.setAttribute('src', '/assets/codepen-white.png')
+    linkedinIcon.setAttribute('src', '/assets/linkedin-white.png')
 
 })
 
@@ -24,14 +26,17 @@ colorSwitch.addEventListener('click', () => {
     root.style.setProperty('--accent-rgb', '0, 179, 60');
 })
 
-const navLinks = document.querySelectorAll('.nav-links a');
+const animLinks = document.querySelectorAll('.animated-link');
 
-navLinks.forEach(link => {
+animLinks.forEach(link => {
     var oldText = link.innerHTML;
     var newText = "";
     link.innerHTML = ""
     for (let i = 0; i < oldText.length; i++) {
-        newText += `<span data-text='${oldText[i]}' class='animated-text letter-${i}'>${oldText[i]}</span>`;
+        if (oldText[i] == " ") 
+            newText += `<span data-text='&nbsp;' class='animated-text letter-${i}'>&nbsp;</span>`;
+        else
+            newText += `<span data-text='${oldText[i]}' class='animated-text letter-${i}'>${oldText[i]}</span>`;
     };
     link.innerHTML = newText;
 })

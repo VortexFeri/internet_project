717dc123d0c04f2a96e233e4fe08a29a9ffd4061
Ferencz Carnu <fericarnu@gmail.com>
2022-05-21 23:43:51 +0300

    Finished the contact page and added function to look for cookie

--- src/app.js
+++ src/app.js
@@ -1,40 +1,24 @@
-const themeSwitch = document.querySelector('#theme-switch input');
-const themelabel = document.getElementById('theme-switch');
-const colorSwitch = document.getElementById('color-switch');
-const header = document.querySelector('header');
-const root = document.querySelector(':root');
-const arrow = document.querySelector('#scroll-link img');
-const icons = document.querySelectorAll('.icon');
+// THEME SWITCHER
+
+const themeSwitch = document.querySelector('#theme-switch input'),
+    colorSwitch = document.getElementById('color-switch'),
+    root = document.querySelector(':root')
 
 function switchTheme() {
-    if (themeSwitch.checked) {
-        themeSwitch.classList.add('dark');
-        themelabel.classList.add('dark');
+    if (getCookie('dark') == 'true') {
         root.classList.add('dark');
-        arrow != null ? arrow.setAttribute('src', 'assets/arrow-light.png') : '';
-        header != null ? header.style.setProperty('background-image', 'url("assets/home-dark.jpg")') : '';
-        icons != null ? icons.forEach(icon => {
-            icon.style.filter = 'invert(1)';
-        }) : '';
     }
     else {
-        themeSwitch.classList.remove('dark');
-        themelabel.classList.remove('dark');
         root.classList.remove('dark');
-        arrow != null ? arrow.setAttribute('src', 'assets/arrow-dark.png') : '';
-        header != null ? header.style.setProperty('background-image', 'url("assets/home-light.jpg")') : '';
-        icons != null ? icons.forEach(icon => {
-            icon.style.filter = 'unset';
-        }) : '';
     }
 }
 
 colorSwitch.addEventListener('click', () => {
-    if (root.getAttribute('data-color-theme') == 'magenta' ||
-        root.getAttribute('data-color-theme') == null)
+    if (getCookie('color') == 'magenta')
         root.setAttribute('data-color-theme', 'green');
     else
-        root.setAttribute('data-color-theme', 'magenta')
+        root.setAttribute('data-color-theme', 'magenta');
+    storeColor();
 })
 
 const animLinks = document.querySelectorAll('.animated-link');
@@ -53,8 +37,11 @@ animLinks.forEach(link => {
 })
 
 function storeDark() {
-    document.cookie = `dark=${themeSwitch.checked}`;
+    document.cookie = `dark=${themeSwitch.checked};`;
 };
+function storeColor() {
+    document.cookie = `color=${(root.getAttribute('data-color-theme'))};`
+}
 
 themeSwitch.addEventListener('change', function () {
     storeDark();
@@ -62,13 +49,15 @@ themeSwitch.addEventListener('change', function () {
 });
 
 window.onload = () => {
-    if (document.cookie == 'dark=true') {
+    if (getCookie('dark') == 'true') {
         themeSwitch.setAttribute('checked', true);
     }
     else {
         themeSwitch.removeAttribute('checked');
     }
     switchTheme();
+
+    root.setAttribute('data-color-theme', getCookie('color'))
 }
 
 const form = document.querySelector('form');
@@ -89,4 +78,28 @@ function dismiss() {
             overlay.style.display = 'none';
         }, 200
     )
+}
+
+function getCookie(name) {
+    let cookieArray = document.cookie.split(';');
+
+    for (var i = 0; i < cookieArray.length; i++) {
+        var curCookie = cookieArray[i].split('=');
+
+        if(name == curCookie[0].trim()) {
+            return decodeURIComponent(curCookie[1]);
+        }
+    }
+
+    return null;
+}
+
+const projectCard = document.querySelectorAll('.project-card');
+const iframe = document.querySelectorAll('iframe');
+
+for(let i = 0; i < projectCard.length; i++) {
+    projectCard[i].addEventListener('click', () => {
+        iframe[i].classList.toggle('hidden');
+        projectCard[i].classList.toggle('hidden');
+    })
 }
